388b6956f92e219e1ae4e5cc7580364e2f569997
unknown <logplayviews@gmail.com>
2025-03-11 20:08:20 -0300

    Feat: Implementação da edição de administradores da aplicação

--- src/main/java/com/web/tornese/SpringWeb/controllers/AdministradoresController.java
+++ src/main/java/com/web/tornese/SpringWeb/controllers/AdministradoresController.java
@@ -1,6 +1,7 @@
 package com.web.tornese.SpringWeb.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -39,5 +40,33 @@ public class AdministradoresController {
         repo.deleteById(id);
         return "redirect:/administradores";
     }
+
+    @GetMapping("/administradores/{id}")
+        public String busca(@PathVariable int id, Model model) {
+        Optional<Administrador> admin = repo.findById(id);
+        try{
+            model.addAttribute("administrador", admin.get());
+        }
+        catch(Exception err){ return "redirect:/administradores"; }
+        
+        return "/administradores/editar";
+    }
+
+    @PostMapping("/administradores/{id}/atualizar")
+        public String atualizar(@PathVariable int id, Administrador administrador) {
+
+            if(!repo.existsById(id)) {
+                return "redirect:/administradores";
+            }
+            Administrador existente = repo.findById(id).orElseThrow(() -> new IllegalArgumentException("Administrador não encontrado"));
+            existente.setNome(administrador.getNome());
+            existente.setEmail(administrador.getEmail());
+            existente.setSenha(administrador.getSenha());
+            existente.setObservacao(administrador.getObservacao());
+
+            repo.save(existente);
+
+            return "redirect:/administradores";
+        }
 
 }
