f09fb527ea35e86deb31cf3638498e1deb41101d
unknown <logplayviews@gmail.com>
2025-03-13 14:40:58 -0300

    Feat: implementação dos cookies e da segurança da aplicação

--- src/main/java/com/web/tornese/SpringWeb/controllers/LoginController.java
+++ src/main/java/com/web/tornese/SpringWeb/controllers/LoginController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.web.tornese.SpringWeb.models.Administrador;
 import com.web.tornese.SpringWeb.repositorio.AdministradoresRepo;
+import com.web.tornese.SpringWeb.servico.CookieService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.ui.Model;
 
@@ -21,13 +24,25 @@ public class LoginController {
     }
 
     @PostMapping("/logar")
-    public String logar(Model model, Administrador admParam, String lembrar) {
+    public String logar(Model model, Administrador admParam, String lembrar, HttpServletResponse response) {
         Administrador adm = this.repo.login(admParam.getEmail(), admParam.getSenha());
 
         if(adm != null){
+            int tempoLogado = 60*60;
+            if(lembrar != null){
+                tempoLogado = 60*60*24*365;
+            }
+            CookieService.SetCookie(response,"usuarioId", String.valueOf(adm.getId()),tempoLogado);
             return "redirect:/";
         }
         model.addAttribute("erro", "Usuário ou senha inválidos");
         return "login/index";
+    }
+
+    @GetMapping("/sair")
+    public String logar(HttpServletResponse response) {
+        
+        CookieService.SetCookie(response,"usuarioId", "",0);
+        return "redirect:/login";
     }
 }
